2a07e59429144c21f1ee99a2aa9b2c1d3ca89a8f
unknown <henri.sellisrgmail.com>
2020-03-22 17:01:57 +0200

    updated presenter client

---
+++ src/main/java/presenterclient/PresenterClientBackEnd.java
@@ -0,0 +1,40 @@
+package presenterclient;
+
+import javafx.stage.Stage;
+
+import java.util.concurrent.BlockingQueue;
+
+
+public class PresenterClientBackEnd implements Runnable {
+    Stage primaryStage;
+    BlockingQueue<Integer> questionTypes;
+    BlockingQueue<String> questions;
+
+    public PresenterClientBackEnd(Stage primaryStage, BlockingQueue<Integer> questionTypes, BlockingQueue<String> questions) {
+        this.primaryStage = primaryStage;
+        this.questionTypes = questionTypes;
+        this.questions = questions;
+
+    }
+
+    public void run() {
+        System.out.println("Back-end started");
+
+        try{
+            // kui back-end saab serverilt käsu näidata järgmist küsimust, siis paneb vajaliku info BlockingQueue-desse ära ja siis (mingil viisil) triggerib GUI threadi showNextQuestion() meetodi
+            Thread.sleep(1000);
+            questionTypes.add(Integer.valueOf(0));
+            questions.add("Nimeta riik Lõuna-Ameerikas");
+
+            Thread.sleep(2000);
+            questionTypes.add(1);
+            questions.add("Mis objekti on kujutatud antud pildil?");
+
+        }
+        catch (InterruptedException e){
+            e.printStackTrace();
+        }
+
+
+    }
+}
