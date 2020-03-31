5cc40b31f7137d32a4011cf5bf13a57a9c80f0d6
Henri Sellis <henri.sellis@gmail.com>
2020-03-31 16:47:24 +0300

    added hashPassword method to UsersDatabaseLayer

--- src/main/java/general/User.java
+++ src/main/java/general/User.java
@@ -1,5 +1,11 @@
 package general;
 
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+
 public class User {
 
     private int id;
